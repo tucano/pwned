e0fb50859e859ff5ccf428eeb7487ce71466b74c
tucano <davide.rambaldi@gmail.com>
2009-03-27 14:43:34 +0100

    Coverage rake task is now optional (depend on presence of gem). In addition: annotationservice first validations on numebr of elements and URI.parse

--- app/models/annotationservice.rb
+++ app/models/annotationservice.rb
@@ -1,3 +1,4 @@
+# FORMAT: ID LABEL URL
 class Annotationservice
   
   def initialize(data)
@@ -6,7 +7,20 @@ class Annotationservice
   
   def valid?
     return false if @data.nil?
-    true
+    @data.each_line do |line|
+      line.chomp
+      notes = line.split
+      return false unless (notes.size > 1 and notes.size < 4)
+      # try to parse notes[2] as URI
+      if notes[2] then
+        begin 
+          URI.parse(notes[2])
+        rescue URI::InvalidURIError
+          return false
+        end
+      end
+    end
+    return true
   end
   
 end
